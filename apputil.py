c4435feda8c13799b751732530d3ffcb05bfb1d2
divyakapoor23 <divya.kapoor878@gmail.com>
2025-10-16 12:46:26 -0400

    Exercise 1

--- apputil.py
+++ apputil.py
@@ -8,12 +8,32 @@ class MarkovText(object):
         self.term_dict = None  # you'll need to build this
 
     def get_term_dict(self):
-
-        # your code here ...
-
-        self.term_dict = {}
-
-        return None
+        """
+        Build a transition dictionary for the Markov chain.
+        
+        Keys: unique tokens in the corpus
+        Values: lists of all tokens that follow each key
+        
+        We include duplicates in the lists because they represent the frequency
+        of transitions, which is important for maintaining the probabilistic
+        nature of the Markov chain.
+        """
+        # Use defaultdict(list) to automatically create empty lists for new keys
+        term_dict = defaultdict(list)
+        
+        # Split the corpus into tokens
+        tokens = self.corpus.split()
+        
+        # Build the transition dictionary
+        for i in range(len(tokens) - 1):
+            current_token = tokens[i]
+            next_token = tokens[i + 1]
+            term_dict[current_token].append(next_token)
+        
+        # Convert to regular dict and store
+        self.term_dict = dict(term_dict)
+        
+        return self.term_dict
 
 
     def generate(self, seed_term=None, term_count=15):
